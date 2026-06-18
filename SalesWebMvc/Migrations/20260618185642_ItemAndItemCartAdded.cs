587586acfc42a65108f8acc1146a5c59e2dfb166
DiogomgTrindade <diogovsky1904@gmail.com>
2026-06-18 19:59:30 +0100

    feat: Add Item, ItemCart Update Department and SalesRecord classes to get these classes

---
+++ SalesWebMvc/Migrations/20260618185642_ItemAndItemCartAdded.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class ItemAndItemCartAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Seller",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    DepartmentId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Item_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemCart",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    ItemId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    SalesRecordId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemCart", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemCart_Item_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Item",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemCart_SalesRecord_SalesRecordId",
+                        column: x => x.SalesRecordId,
+                        principalTable: "SalesRecord",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_DepartmentId",
+                table: "Item",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemCart_ItemId",
+                table: "ItemCart",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemCart_SalesRecordId",
+                table: "ItemCart",
+                column: "SalesRecordId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemCart");
+
+            migrationBuilder.DropTable(
+                name: "Item");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ SalesWebMvc/Models/ItemCart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMvc.Models
+{
+    public class ItemCart
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+
+        public ItemCart()
+        {
+        }
+
+        public ItemCart(int id, int itemId, Item item, int quantity)
+        {
+            Id = id;
+            ItemId = itemId;
+            Item = item;
+            Quantity = quantity;
+        }
+
+        public double TotalValue()
+        {
+            return Item.Price * Quantity;
+        }
+    }
+}
